7258dc9e60a27d1fe0321bfdab7a8c380a83a8f1
subham.jalan@plaksha.edu.in <subham.jalan@plaksha.edu.in>
2025-01-01 18:37:07 +0530

    added boilerplate for the node

--- auv_sensing/src/sense.cpp
+++ auv_sensing/src/sense.cpp
@@ -1,3 +1,17 @@
-int main() {
+#include "rclcpp/rclcpp.hpp"
+
+class SenseNode : public rclcpp::Node
+{
+public:
+    SenseNode() : Node("auv_sense") {}
+private:
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SenseNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 }
