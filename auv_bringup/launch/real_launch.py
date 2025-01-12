c36f021d149b79e2b91ba32bd83f8f13b464f991
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-01-12 20:54:25 +0530

    feat: create launch file to connect mavros with real AUV

---
+++ auv_bringup/launch/real_launch.py
@@ -0,0 +1,32 @@
+"""
+Launch a connection to the real robot.
+
+Includes MAVROS
+"""
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import (
+    PathJoinSubstitution,
+    LaunchConfiguration,
+)
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_auv_bringup = get_package_share_directory("auv_bringup")
+    mavros_params_file = PathJoinSubstitution(
+        [pkg_auv_bringup, "params", "real_mavros_params.yaml"]
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package="mavros",
+                executable="mavros_node",
+                output="screen",
+                parameters=[mavros_params_file],
+            ),
+        ]
+    )
