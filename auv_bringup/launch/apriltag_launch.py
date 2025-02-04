6e640480642f792f3da29c4fcb46ea243440fab6
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-02-04 15:55:38 +0530

    feat: create apriltag launch file w correct params

---
+++ auv_bringup/launch/apriltag_launch.py
@@ -0,0 +1,32 @@
+"""
+Launch an AprilTag detector.
+
+Includes MAVROS
+"""
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import (
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_auv_bringup = get_package_share_directory("auv_bringup")
+    mavros_params_file = PathJoinSubstitution(
+        [pkg_auv_bringup, "params", "apriltag_params.yaml"]
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package="apriltag_ros",
+                executable="apriltag_node",
+                output="screen",
+                parameters=[mavros_params_file],
+                remappings=[
+                    ('/image_rect','/image_raw')
+                ]
+            ),
+        ]
+    )
