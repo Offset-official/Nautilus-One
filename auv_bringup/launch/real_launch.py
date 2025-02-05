c3923e3cdff1725ee9870d72ccaa261e946ff74c
subham.jalan@plaksha.edu.in <subham.jalan@plaksha.edu.in>
2025-02-05 23:52:55 +0530

    mine

--- auv_bringup/launch/real_launch.py
+++ auv_bringup/launch/real_launch.py
@@ -1,21 +1,11 @@
 """
-<<<<<<< HEAD
-Launch mavros for real robot.
-=======
 Launch a connection to the real robot.
->>>>>>> 40bdb0b769a7915cd255f5aadc91a18d7e500afa
 
 Includes MAVROS
 """
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-<<<<<<< HEAD
-from launch.actions import (
-    DeclareLaunchArgument,
-)
-=======
->>>>>>> 40bdb0b769a7915cd255f5aadc91a18d7e500afa
 from launch.substitutions import (
     PathJoinSubstitution,
     LaunchConfiguration,
