efbf3945e8aa200d0ea66f69fa8b8e3f6f755710
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-02-14 21:04:40 +0530

    print detection simulatenously

--- auv_bringup/launch/apriltag_launch.py
+++ auv_bringup/launch/apriltag_launch.py
@@ -6,6 +6,7 @@ Includes MAVROS
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch.substitutions import (
     PathJoinSubstitution,
 )
@@ -13,7 +14,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     pkg_auv_bringup = get_package_share_directory("auv_bringup")
-    mavros_params_file = PathJoinSubstitution(
+    apriltag_params_file = PathJoinSubstitution(
         [pkg_auv_bringup, "params", "apriltag_params.yaml"]
     )
 
@@ -23,10 +24,14 @@ def generate_launch_description():
                 package="apriltag_ros",
                 executable="apriltag_node",
                 output="screen",
-                parameters=[mavros_params_file],
+                parameters=[apriltag_params_file],
                 remappings=[
                     ('/image_rect','/image_raw')
                 ]
+            ),
+            ExecuteProcess(
+                cmd=['ros2', 'topic', 'echo', '/topic_name'],
+                output='screen', 
             ),
         ]
     )
