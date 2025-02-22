181835b441eef92ca3b58c3408446e60d137add0
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-02-23 02:26:56 +0530

    subscribe to image

--- auv_comms/src/read_sequence.cpp
+++ auv_comms/src/read_sequence.cpp
@@ -1,16 +1,18 @@
 #include "auv_interfaces/action/read_comm_sequence.hpp"
+#include "auv_interfaces/srv/image_color_detect.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
+#include "sensor_msgs/msg/compressed_image.hpp"
 #include <functional>
+#include <image_transport/image_transport.hpp>
 #include <memory>
 #include <thread>
 
 class ReadCommSequenceActionServer : public rclcpp::Node {
 public:
   using ReadCommSequence = auv_interfaces::action::ReadCommSequence;
-  using GoalHandleReadCommSequence =
-      rclcpp_action::ServerGoalHandle<ReadCommSequence>;
+  using GoalHandleReadCommSequence = rclcpp_action::ServerGoalHandle<ReadCommSequence>;
+  using ImageColorDetect = auv_interfaces::srv::ImageColorDetect;
 
   explicit ReadCommSequenceActionServer(
       const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
@@ -22,10 +24,21 @@ public:
         std::bind(&ReadCommSequenceActionServer::handle_goal, this, _1),
         std::bind(&ReadCommSequenceActionServer::handle_cancel, this, _1),
         std::bind(&ReadCommSequenceActionServer::handle_accepted, this, _1));
+
+    this->color_detect_client_ =
+        this->create_client<ImageColorDetect>("image_color_detect_client");
+
+    this->image_sub_ = image_transport::create_subscription(
+        this, "input_image",
+        std::bind(&ReadCommSequenceActionServer::image_callback, this, _1), "compressed",
+        rclcpp::SensorDataQoS().get_rmw_qos_profile());
   }
 
 private:
   rclcpp_action::Server<ReadCommSequence>::SharedPtr action_server_;
+  rclcpp::Client<ImageColorDetect>::SharedPtr color_detect_client_;
+  image_transport::Subscriber image_sub_;
+  sensor_msgs::msg::Image::ConstSharedPtr msg_;
 
   rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID &uuid) {
     RCLCPP_INFO(this->get_logger(), "Received goal request");
@@ -38,6 +51,10 @@ private:
     RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
     (void)goal_handle;
     return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr &msg) {
+    msg_ = msg;
   }
 
   void handle_accepted(
@@ -64,6 +81,7 @@ private:
         RCLCPP_INFO(this->get_logger(), "Goal canceled");
         return;
       }
+
       sequence.push_back(i);
       goal_handle->publish_feedback(feedback);
       RCLCPP_INFO(this->get_logger(), "Publish feedback");
@@ -79,8 +97,7 @@ private:
   }
 };
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
   auto action_server = std::make_shared<ReadCommSequenceActionServer>();
