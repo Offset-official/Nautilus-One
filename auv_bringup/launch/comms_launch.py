694d06c0a864e85a22063dba9217e7ae4d9a8acf
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-02-23 20:47:28 +0530

    add option to launch pub in launcfile

--- auv_bringup/launch/comms_launch.py
+++ auv_bringup/launch/comms_launch.py
@@ -5,8 +5,10 @@ Launch nodes for communication reception.
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import UnlessCondition,IfCondition
 from launch.substitutions import (
-    PathJoinSubstitution,
+    PathJoinSubstitution,LaunchConfiguration
 )
 from launch_ros.actions import Node
 
@@ -19,18 +21,33 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                'debug',
+                default_value='False',
+                description="Only launch the color detector?"
+            ),
             Node(
                 package="auv_comms",
                 executable="color_detector_server",
                 output="screen",
                 parameters=[color_detector_params_file],
+                condition=UnlessCondition(LaunchConfiguration("debug")),
             ),
             Node(
                 package="auv_comms",
                 executable="read_sequence_server",
                 output="screen",
                 parameters=[color_detector_params_file],
-                remappings=[ ('/input_image/compressed','/image_raw/compressed') ]
+                remappings=[('/input_image/compressed','/image_raw/compressed')],
+                condition=UnlessCondition(LaunchConfiguration("debug")),
+            ),
+            Node(
+                package="auv_comms",
+                executable="color_detector_pub",
+                output="screen",
+                parameters=[color_detector_params_file],
+                remappings=[('/input_image/compressed','/image_raw/compressed')],
+                condition=IfCondition(LaunchConfiguration("debug")),
             ),
         ]
     )
