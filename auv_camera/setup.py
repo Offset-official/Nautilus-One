086721d87a4d2c8ac1d459f11fd982f71b89099a
Anshuman Sharma <69304992+and-human@users.noreply.github.com>
2025-03-04 02:13:28 +0530

    added camera_launch file

--- auv_camera/setup.py
+++ auv_camera/setup.py
@@ -11,7 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        # (os.path.join("share", package_name, "models"), glob("models/*")),
+        ('share/' + package_name + '/launch', ['launch/cameras_launch.py'])
     ],
     install_requires=[],
     zip_safe=True,
