e6f0a1bbc0b2c92649e45e1d0744ec794f78297e
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-03-04 10:25:42 +0530

    correct launch file name

--- auv_bringup/launch/nano.launch.py
+++ auv_bringup/launch/nano.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
     camera_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(camera_launch_file))
 
     diagnostics_launch_file = os.path.join(get_package_share_directory('auv_diagnostics'),'launch','diagnostics_launch.py')
-    diagnostics_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(camera_launch_file))
+    diagnostics_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(diagnostics_launch_file))
 
     return LaunchDescription([
         camera_launch,diagnostics_launch
