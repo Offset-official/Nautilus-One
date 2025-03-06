ead06b4f7e09077065b97245c4bd0ef45a5929f8
Abhinav <this.abhinav26@gmail.com>
2025-03-06 11:38:32 +0530

    I might be the most dumb person out there

--- auv_controller/src/controller_actions.cpp
+++ auv_controller/src/controller_actions.cpp
@@ -72,7 +72,7 @@ void DumbController::execute(
     result->final_depth = depth;
     goal_handle->succeed(result);
     RCLCPP_INFO(this->get_logger(), "Goal succeeded");
-    color_request->color = "#FFFFFFF";
+    color_request->color = "#FFFFFF";
     req_ = led_color_client_->async_send_request(color_request);
     req_.wait();
   }
