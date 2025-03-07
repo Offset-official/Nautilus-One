e988f245606d17ef0f4b771dbc4b27955c0c6cb8
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-03-07 14:34:33 +0530

    create gate bt node

---
+++ auv_bt/include/auv_bt/Gate.hpp
@@ -0,0 +1,42 @@
+#ifndef AUV_BT_GATE_HPP_
+#define AUV_BT_GATE_HPP_
+
+#include <string>
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#include "auv_interfaces/msg/detection_array.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+class Gate : public BT::ConditionNode {
+public:
+  explicit Gate(const std::string &xml_tag_name,
+                const BT::NodeConfiguration &conf);
+
+  BT::NodeStatus tick();
+
+  static BT::PortsList providedPorts() {
+    return BT::PortsList({
+        BT::InputPort<double>("size"),
+        BT::InputPort<std::string>("mode"), // should either be max or min
+        BT::OutputPort<int>("num"), // number of leds which should be lit up
+    });
+  }
+
+  void detections_callback(auv_interfaces::msg::DetectionArray::UniquePtr msg);
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Time last_reading_time_;
+  rclcpp::Subscription<auv_interfaces::msg::DetectionArray>::SharedPtr
+      detections_sub_;
+  auv_interfaces::msg::DetectionArray::UniquePtr last_detections_;
+  double last_computed_size;
+  int last_num_leds;
+  const int max_leds = 20;
+
+  int num_leds_to_turn_on(double, double);
+};
+
+#endif
