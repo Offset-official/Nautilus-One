0aec0631fb91450ca28bd54c7e55a06a04a429b6
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-03-13 16:39:47 +0800

    accept params for tetherless operation

--- auv_bt/src/bt_qualify_main.cpp
+++ auv_bt/src/bt_qualify_main.cpp
@@ -15,6 +15,9 @@ int main(int argc, char *argv[]) {
 
   auto node = rclcpp::Node::make_shared("qualifying_node");
 
+  node->declare_parameter("on_device", false);
+  auto on_device = node->get_parameter("on_device").as_bool();
+
   BT::BehaviorTreeFactory factory;
   BT::SharedLibrary loader;
 
@@ -25,13 +28,23 @@ int main(int argc, char *argv[]) {
   factory.registerFromPlugin(loader.getOSName("auv_set_led_color_bt_node"));
   factory.registerFromPlugin(loader.getOSName("auv_set_mode_bt_node"));
   factory.registerFromPlugin(loader.getOSName("auv_is_armed_bt_node"));
-  factory.registerFromPlugin(loader.getOSName("auv_is_depth_calibrated_bt_node"));
+  factory.registerFromPlugin(
+      loader.getOSName("auv_is_depth_calibrated_bt_node"));
   factory.registerFromPlugin(loader.getOSName("auv_calibrate_depth_bt_node"));
   factory.registerFromPlugin(loader.getOSName("auv_gate_bt_node"));
   factory.registerFromPlugin(loader.getOSName("auv_follow_gate_bt_node"));
   std::string pkgpath = ament_index_cpp::get_package_share_directory("auv_bt");
-  std::string xml_file = pkgpath + "/behavior_tree_xml/gate_detection_forward_follow_test.xml";
-  //std::string xml_file = pkgpath + "/behavior_tree_xml/gate_detection_test.xml";
+  std::string xml_file;
+
+  if (on_device) {
+    xml_file = pkgpath + "/trees/elapsed_descend_test.xml";
+  } else {
+    xml_file =
+        pkgpath + "/behavior_tree_xml/gate_detection_forward_follow_test.xml";
+    // xml_file = pkgpath + "/behavior_tree_xml/gate_detection_test.xml";
+  }
+  RCLCPP_INFO(node->get_logger(), "reading behavior tree from file at : %s",
+              xml_file.c_str());
 
   auto blackboard = BT::Blackboard::create();
   blackboard->set("node", node);
